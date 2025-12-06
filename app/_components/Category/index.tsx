7ece5c41db955454b9bfabf7d2350c937b1afc85
yokota-daichi <ktc25a31g0013@edu.kyoto-tech.ac.jp>
2025-12-06 11:32:45 +0900

    p94

--- app/_components/Category/index.tsx
+++ app/_components/Category/index.tsx
@@ -2,7 +2,7 @@ import type { Category } from "@/app/_libs/microcms";
 import styles from "./index.module.css";
 
 type Props = {
-  category: Category[];
+  category: Category;
 };
 
 export default function Category({ category }: Props) {
